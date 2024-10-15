66a2bba50224dc1bc47c87e730e69a55a8390bc6
leon <leon.colaco@student.saedubai.com>
2024-10-16 01:41:20 +0400

    Added sprites

--- Toys and Activity/Assets/Scripts/MouseHandler.cs
+++ Toys and Activity/Assets/Scripts/MouseHandler.cs
@@ -8,32 +8,32 @@ public class ClickObject : MonoBehaviour
 {
     public GameObject Lever;
     public GameObject Ball;
-    private int flag = 1;
+    //private int flag = 1;
+    //private Vector3 PointA;
+    //private Vector3 PointB;
 
+    private int Rotations;
+    private int count = 0;
     void Start()
     {
-
+        Rotations = Random.Range(5, 21);
     }
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            if (Ball == GetClickedObject(out RaycastHit hit))
+            if (Lever == GetClickedObject(out RaycastHit hit))
             {
-                if (flag == 1)
+                if (count == Rotations)
                 {
-                    flag = 2;
+
                 }
-                if (flag == 2)
+                else
                 {
-                    flag = 1;
+                    count++;
                 }
             }
-        }
-        if (Input.GetMouseButtonUp(0))
-        {
-            
-        }
+        }      
     }
 
     GameObject GetClickedObject(out RaycastHit hit)
@@ -42,11 +42,11 @@ public class ClickObject : MonoBehaviour
         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray.origin, ray.direction * 10, out hit))
         {
-            if (!isPointerOverUIObject()) { target = hit.collider.gameObject; }
+            if (!isPointerOverObject()) { target = hit.collider.gameObject; }
         }
         return target;
     }
-    private bool isPointerOverUIObject()
+    private bool isPointerOverObject()
     {
         PointerEventData ped = new PointerEventData(EventSystem.current);
         ped.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
